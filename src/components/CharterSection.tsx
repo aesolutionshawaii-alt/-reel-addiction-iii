33b0feae620f7c02ffb0c418276c1464e5d82837
aesolutionshawaii-alt <aesolutionshawaii@gmail.com>
2025-12-16 12:10:16 -1000

    Update CharterSection.tsx

--- src/components/CharterSection.tsx
+++ src/components/CharterSection.tsx
@@ -65,13 +65,12 @@ export default function CharterSection() {
                     {/* Fixed 900px image - card clips it */}
                     <div className={`absolute top-0 ${charter.position === 'left' ? 'left-0' : 'right-0'} w-[820px] h-full`}>
                       <Image
-                        src={charter.image}
-                        alt={charter.title}
-                        fill
-                        className="object-cover"
-                        quality={90}
-                      />
-                    </div>
+  src={charter.image}
+  alt={charter.title}
+  fill
+  className="object-cover object-center"
+  quality={90}
+/>
                     
                     {/* Top gradient */}
                     <div 
