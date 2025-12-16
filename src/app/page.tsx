062d8a2ecec6399f10d65352a7ad6706d6f0b164
aesolutionshawaii-alt <aesolutionshawaii@gmail.com>
2025-12-15 16:39:55 -1000

    Add SocialSidebar component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import Testimonial from '@/components/Testimonial'
 import SeaKeeperSection from '@/components/SeaKeeperSection'
 import FinalCTA from '@/components/FinalCTA'
 import Footer from '@/components/Footer'
+import SocialSidebar from '@/components/SocialSidebar'
 
 export default function Home() {
   return (
     <main>
+      <SocialSidebar />
       <Hero />
       <ExploreCards />
       <CharterSection />
