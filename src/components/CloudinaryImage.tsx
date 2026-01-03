23219a9d2d241dc6cb718df23c9efb3d0ffedaad
Josh Shuman <joshshuman@Mac.lan>
2026-01-03 12:16:33 -1000

    Cloudinary optimization - AVIF format, native lazy loading

--- src/components/CloudinaryImage.tsx
+++ src/components/CloudinaryImage.tsx
@@ -2,22 +2,13 @@
 
 import { CldImage, CldImageProps } from 'next-cloudinary';
 
-const CLOUD_NAME = 'dmu9szrap';
-
-type CloudinaryImageProps = Omit<CldImageProps, 'placeholder' | 'blurDataURL'> & {
-  skipBlur?: boolean;
-};
-
-export default function CloudinaryImage({ src, skipBlur = false, ...props }: CloudinaryImageProps) {
-  // Generate tiny blurred version URL for placeholder
-  const blurDataURL = `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/w_10,q_1,e_blur:1000/${src}`;
-
+export default function CloudinaryImage(props: CldImageProps) {
   return (
     <CldImage
-      src={src}
-      placeholder={skipBlur ? undefined : 'blur'}
-      blurDataURL={skipBlur ? undefined : blurDataURL}
       {...props}
+      format="avif"
+      loading={props.priority ? 'eager' : 'lazy'}
+      decoding="async"
     />
   );
 }
