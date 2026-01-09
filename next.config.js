49b7c262206c5fae261e7d2a5235f651ef24b614
Josh Shuman <joshshuman@Mac.lan>
2026-01-09 07:10:56 -1000

    perf: add aggressive cache headers for video files

--- next.config.js
+++ next.config.js
@@ -15,13 +15,25 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-
   async redirects() {
     return [
       {
         source: '/private-charter',
         destination: '/custom',
         permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: '/videos/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
       },
     ];
   },
